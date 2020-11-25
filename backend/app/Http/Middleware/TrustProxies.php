30cb878e8ed7606b2376ab919a009e397b1cc84f
D3v01dZA <caltona1@gmail.com>
2020-11-24 21:16:32 -0500

    Correctly make a String

--- backend/app/Http/Middleware/TrustProxies.php
+++ backend/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var string|array
      */
-    protected $proxies = *;
+    protected $proxies = "*";
 
     /**
      * The headers that should be used to detect proxies.
